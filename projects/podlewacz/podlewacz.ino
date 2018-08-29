c3f3a36c6d88f54be8a5ea3a0760703da3531134
mar33cki <mar33cki@gmail.com>
2018-08-29 09:04:28 +0200

    poprawki kodu, weryfikacja na procku po serialu bez urządzeń

--- projects/podlewacz/podlewacz.ino
+++ projects/podlewacz/podlewacz.ino
@@ -4,9 +4,10 @@
 
 //variables
 // constants won't change:
-const int RainSensor = A0;  //rain sensor
+const int RainSensorPin = A0;  //rain sensor
+int RainSensor = 0;
 const int outPins[] = { 5, 6, 7, 8, 9, 10, 11, 12};       // an array of output Pins, D2,...
-int outStates[] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW}; //array of output pins states
+int outStates[] = {HIGH, LOW, LOW, LOW, LOW, LOW, LOW, LOW}; //array of output pins states
 const int pinCount = 8;           // the number of pins (i.e. the length of the array)
 
 // Generally, you should use "unsigned long" for variables that hold time
@@ -26,30 +27,50 @@ void SetAllOff()
     digitalWrite(outPins[thisPin], LOW);
   }
   delay(100);
+  Serial.println(F("SetAllOff()"));
 }
 //--------------------------------
-
+//void SetPinState(int Pin; int State)
+//{
+//  digitalWrite(Pin, state);
+//  delay(50);
+//  Serial.print(F("Pin: ")); Serial.print(Pin);Serial.print(F(" : "));  Serial.println(state);
+//  
+//}
+//--------------------------------
 void setup() {
   // put your setup code here, to run once:
+  // initialize the serial communication:
+  Serial.begin(9600);
   //input pin(s)
-  pinMode(RainSensor, INPUT);
+  Serial.println(F("setup()"));
+  pinMode(RainSensor, INPUT_PULLUP);
   // use a for loop to initialize each pin as an output:
   for (int thisPin = 0; thisPin < pinCount; thisPin++) 
   {
     pinMode(outPins[thisPin], OUTPUT);
   }
   SetAllOff();
+  //set 1st output ON
+  digitalWrite(outPins[0], outStates[0]);
+  delay(50);
+  Serial.print(F("outPin[0] : "));  Serial.print(outPins[0]); Serial.print(F(" : ")); Serial.println(outStates[0]);
 }
 //--------------------------------
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
   unsigned long currentMillis = millis();
-  static int thisPin = 0; //current pin
+  static int thisPin = 1; //current pin, pin 0 already ON in setup
 
   //rain sensor DO: high - no rain, low - rain
+  RainSensor = digitalRead(RainSensorPin);
+  Serial.print(F("RainSensor: ")); Serial.println(RainSensor);
   if(RainSensor == HIGH) //no rain, normal work - iterate
+  //if(RainSensor == LOW) //no rain, normal work - iterate
   {
     //check elapsed time 
+    Serial.print(F("Millis: ")); Serial.print(currentMillis - previousMillis); Serial.print(F("; interval: ")); Serial.println(interval*MINUTE);
     if (currentMillis - previousMillis >= interval*MINUTE) 
     {
       // save the last time you blinked the LED
@@ -64,11 +85,14 @@ void loop() {
       // turn thisPin ON:
       digitalWrite(outPins[thisPin], outStates[thisPin]);
       delay(50);
+      Serial.print(F("outPin[")); Serial.print(thisPin);Serial.print(F("] : "));  Serial.print(outPins[thisPin]); Serial.print(F(" : ")); Serial.println(outStates[thisPin]);
   
       //evaluate thisPin value
       if(thisPin >= pinCount) thisPin = 0; //reset to zero
       else thisPin++;  //increment
+      Serial.print(F("thisPin: ")); Serial.println(thisPin);
     }
-    else thisPin = 0; //rain, do nothing
   }
+  else thisPin = 0; //rain, do nothing
+  delay(1000);  //remove
 }
