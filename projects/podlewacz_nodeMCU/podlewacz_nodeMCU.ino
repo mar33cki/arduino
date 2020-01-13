68026560360510f475856435167087529ed3c56a
mar33cki <mar33cki@gmail.com>
2020-01-13 09:30:28 +0100

    add more serial debug info, modify weather conditions check

--- projects/podlewacz_nodeMCU/podlewacz_nodeMCU.ino
+++ projects/podlewacz_nodeMCU/podlewacz_nodeMCU.ino
@@ -29,8 +29,8 @@
 #define D10 1 // TX0 (Serial console)
 
 #define OUTPUTS 4 //max loop index
-#define StartHour 07
-#define StartMinute 00
+#define StartHour 14
+#define StartMinute 05
 #define WateringTime 10
 #define JSON_BUFF_DIMENSION 2500
 
@@ -40,14 +40,14 @@ int ForceWatering = D5; //user button to force the watering
 int ForceWateringState = 0, PreviousForceWateringState = 0; //user button state
 
 const char *ssid     = "ssid";
-const char *password = "pass";
+const char *password = "password";
 
 WiFiUDP ntpUDP;
 int hour, minute;
 unsigned long  TimerMinutes = 0, previousMinute = 0;
 
 //europe server, utcOffsetInSeconds (+1:3600, +2:7200), updateInterval (10s)
-NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 7200, 60000);
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
 String text;
 int jsonend = 0;
 boolean startJson = false;
@@ -60,6 +60,7 @@ struct Weather_Data
   int main_humidity;
   float main_temp_min;
   float main_temp_max;
+  int actual;
 };
 Weather_Data Weather;
 
@@ -98,8 +99,11 @@ void watering()
   if(PreviousForceWateringState != ForceWateringState) { Serial.print(F("ForceWateringState: ")); Serial.println(ForceWateringState);}
   PreviousForceWateringState = ForceWateringState;
 
+  //verify actual weather
+  if(hour == StartHour && minute == StartMinute && Weather.actual == 0) makehttpRequest();
+
   //wait for StartHour:StartMinute and StartWatering or for user button ForceWateringState
-  if( (hour == StartHour && minute == StartMinute && StartWatering == 0 && AllowWatering == 1) || (ForceWateringState == 0) )
+  if( (hour == StartHour && minute == StartMinute && StartWatering == 0 && AllowWatering == 1 && Weather.weather_0_id >= 800) || (ForceWateringState == 0) )
   { 
     StartWatering = 1;
     CurrentTimer = TimerMinutes + WateringTime;
@@ -127,6 +131,7 @@ void watering()
   StartWatering = 0;
   Serial.println(F("Stop the watering! "));
   ind = 0;
+  Weather.actual = 0;
  }
 }
 //---------------------------------------------------------------------
@@ -135,11 +140,13 @@ void makehttpRequest() {
   // close any connection before send a new request to allow client make connection to server
   client.stop();
 
+  Serial.print(server); Serial.print(F("/data/2.5/weather?id=")); Serial.print(idOfCity); Serial.print(F("&APPID=")); Serial.print(apiKey); Serial.println(F("&mode=json&units=metric&cnt=2"));
   // if there's a successful connection:
-  if (client.connect(server, 80)) {
-    // Serial.println("connecting...");
+  if (client.connect(server, 80)) 
+  {
+    //Serial.print(".");
     // send the HTTP PUT request:
-    client.println("GET /data/2.5/forecast?id=" + idOfCity + "&APPID=" + apiKey + "&mode=json&units=metric&cnt=2 HTTP/1.1");
+    client.println("GET /data/2.5/weather?id=" + idOfCity + "&APPID=" + apiKey + "&mode=json&units=metric&cnt=2 HTTP/1.1");
     client.println("Host: api.openweathermap.org");
     client.println("User-Agent: ArduinoWiFi/1.1");
     client.println("Connection: close");
@@ -147,7 +154,8 @@ void makehttpRequest() {
     
     unsigned long timeout = millis();
     while (client.available() == 0) {
-      if (millis() - timeout > 5000) {
+      if (millis() - timeout > 5000) 
+      {
         Serial.println(">>> Client Timeout !");
         client.stop();
         return;
@@ -155,11 +163,13 @@ void makehttpRequest() {
     }
     
     char c = 0;
-    while (client.available()) {
+    text = "";  // clear text string for the next time
+    while (client.available()) 
+    {
       c = client.read();
       // since json contains equal number of open and close curly brackets, this means we can determine when a json is completely received  by counting
       // the open and close occurences,
-      //Serial.print(c);
+      Serial.print(c);
       if (c == '{') {
         startJson = true;         // set startJson true to indicate json message has started
         jsonend++;
@@ -171,7 +181,8 @@ void makehttpRequest() {
         text += c;
       }
       // if jsonend = 0 then we have have received equal number of curly braces 
-      if (jsonend == 0 && startJson == true) {
+      if (jsonend == 0 && startJson == true) 
+      {
         parseJson(text.c_str());  // parse c string text in parseJson function
         text = "";                // clear text string for the next time
         startJson = false;        // set startJson to false to indicate that a new message has not yet started
@@ -188,6 +199,7 @@ void makehttpRequest() {
 //to parse json data recieved from OWM
 void parseJson(const char * jsonString) 
 {
+  Serial.print(F("parseJsonString: ")); Serial.println(jsonString);
   //StaticJsonBuffer<4000> jsonBuffer;
   const size_t bufferSize = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(1) + 2*JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(6) + JSON_OBJECT_SIZE(13) + 410;
   const size_t capacity = bufferSize;
@@ -211,16 +223,24 @@ void parseJson(const char * jsonString)
   //JsonObject& root = jsonBuffer.parseObject(json);
 
   //JsonObject& weather_0 = root["weather"][0];
-  Weather.weather_0_id = jsonBuffer["weather"]["id"]; // 800
-  Weather.weather_0_main = jsonBuffer["weather"]["main"].as<char*>(); // "Clear"
-  Serial.print(F("Weather_id: ")); Serial.print(Weather.weather_0_id); Serial.print(F("; ")); Serial.println(Weather.weather_0_main);
+  Weather.weather_0_id = jsonBuffer["weather"][0]["id"]; // 800
+  Weather.weather_0_main = jsonBuffer["weather"][0]["main"].as<char*>(); // "Clear"
+  Serial.print(F("weather_0_id: ")); Serial.println(Weather.weather_0_id);
+  Serial.print(F("weather_0_main: ")); Serial.println(Weather.weather_0_main);
 
   //JsonObject& main = root["main"];
   Weather.main_temp = jsonBuffer["main"]["temp"]; // 20.47
+  Serial.print(F("temp: ")); Serial.println(Weather.main_temp);
   Weather.main_pressure = jsonBuffer["main"]["pressure"]; // 1014
+  Serial.print(F("main_pressure: ")); Serial.println(Weather.main_pressure);
   Weather.main_humidity = jsonBuffer["main"]["humidity"]; // 40
+  Serial.print(F("main_humidity: ")); Serial.println(Weather.main_humidity);
   Weather.main_temp_min = jsonBuffer["main"]["temp_min"]; // 19.44
+  Serial.print(F("main_temp_min: ")); Serial.println(Weather.main_temp_min);
   Weather.main_temp_max = jsonBuffer["main"]["temp_max"]; // 23.33
+  Serial.print(F("main_temp_max: ")); Serial.println(Weather.main_temp_max);
+  Serial.println(F("----------------"));
+  Weather.actual = 1;
 }
 //---------------------------------------------------------------------
 void setup(){
@@ -242,7 +262,7 @@ void setup(){
     Serial.print ( F(".") );
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   }
-  Serial.println(F("Connected!"));
+  Serial.println(F("Wi-Fi Connected!"));
 
   timeClient.begin();
   Serial.print(F("Watering at: ")); Serial.print(StartHour); Serial.print(F(":")); Serial.print(StartMinute);
@@ -256,7 +276,7 @@ void loop()
   timeClient.update();
 
   Timer();
-  makehttpRequest();
+  //makehttpRequest();
   watering();
 
   delay(1000);
